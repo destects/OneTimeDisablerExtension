a8d5c33d834d866e342e67eca6b342e074e5fc28
destects <destects.travism@gmail.com>
2020-03-12 01:11:57 -0400

    Initial Commit

---
+++ OneTimeDisablerExtension/Commands/DisableCommand.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ExtensionManager;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace OneTimeDisablerExtension.Commands {
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class DisableCommand {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 4130;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("a84f652b-4f12-40bd-9963-34e553920b4b");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+        private readonly IVsExtensionManager _extManager;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DisableCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private DisableCommand(AsyncPackage package, OleMenuCommandService commandService, IVsExtensionManager extenService) {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            _extManager = extenService;
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static DisableCommand Instance {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider {
+            get {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package, IVsExtensionManager extenService) {
+            // Switch to the main thread - the call to AddCommand in DisableCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new DisableCommand(package, commandService, extenService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e) {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if ((this.package as VSPackage).TargetExtensionIdentifier is string tei && !string.IsNullOrWhiteSpace(tei)) {
+
+                if (_extManager.TryGetInstalledExtension(tei, out IInstalledExtension ext)) {
+                    _extManager.Disable(ext);
+                    (this.package as VSPackage).IsInDisableCycle = true;
+
+
+                }
+
+            } else {
+
+                // Show a message box to prove we were here
+                VsShellUtilities.ShowMessageBox(
+                    this.package,
+                    "No Target identifier set",
+                    "One Time Disabler",
+                    OLEMSGICON.OLEMSGICON_CRITICAL,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+        }
+    }
+}
---
+++ OneTimeDisablerExtension/Options/OptionsPageGrid.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+using Task = System.Threading.Tasks.Task;
+using System.ComponentModel;
+
+namespace OneTimeDisablerExtension.Options {
+    public class OptionsPageGrid : DialogPage {
+        [Category("One Time Disabler")]
+        [DisplayName("Target Extension Identifier")]
+        [Description("Identifier of the target extension")]
+        public string TargetExtensionIdentifier {
+            get;
+            set;
+        } = string.Empty;
+        [Category("One Time Disabler")]
+        [DisplayName("Is In Disable Cycle")]
+        [Description("Should the target Extension be scheduled for enabling on next restart")]
+        public bool IsInDisableCycle {
+            get; set;
+        } = false;
+    }
+}
